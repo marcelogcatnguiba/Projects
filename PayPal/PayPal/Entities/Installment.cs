720b5d4a7d23a1c098de972daa920dfb983e896c
Marcelo Gomes <marcelogomes.0104@gmail.com>
2022-11-14 20:08:22 -0300

    Contract logical

---
+++ PayPal/PayPal/Entities/Installment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PayPal.Entities
+{
+    internal class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return $"{DueDate} - {Amount}";
+        }
+    }
+}
---
+++ PayPal/PayPal/Program.cs
@@ -0,0 +1,42 @@
+﻿using PayPal.Entities;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Security.Principal;
+
+namespace PayPal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract value: ");
+            double totalvalue = double.Parse(Console.ReadLine());
+            Console.Write("Enter number of installments: ");
+            int installment = int.Parse(Console.ReadLine());
+
+            Contract contract = new Contract(number, date, totalvalue);
+            for (int i = 1; i <= installment; i++)
+            {
+                DateTime dueDate = contract.Date.AddMonths(i);
+                double amount = contract.TotalValue / installment;
+                Installment installment1 = new Installment(dueDate, amount);
+
+                contract.AddInstallment(installment1);
+
+            }
+
+            foreach(Installment i in contract.Installment)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
