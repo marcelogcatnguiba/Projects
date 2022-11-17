219a88c2dca5e2bf348597d8d81806eef021cc0c
Marcelo Gomes <marcelogomes.0104@gmail.com>
2022-11-17 19:33:15 -0300

    Generics

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,24 @@
+﻿using Generics.Services;
+using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService service = new PrintService();
+            Console.Write("How many values: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+                service.AddValue(value);
+            }
+            service.Print();
+            Console.WriteLine($"First: {service.First()}");
+        }
+    }
+}
