c4b549a1d3e2702323cfa1bc9c315da4950bdfa3
Marcelo Gomes <marcelogomes.0104@gmail.com>
2022-11-21 21:28:06 -0300

    Equals, GetHashCode

---
+++ HashCode/HashCode/Entities/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashCode.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Client))
+            {
+                throw new ArgumentException("Obs is not a client");
+            }
+
+            Client c = obj as Client; // downcasting
+
+            return Email.Equals(c.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ HashCode/HashCode/Program.cs
@@ -0,0 +1,21 @@
+﻿using HashCode.Entities;
+using System;
+using System.Xml;
+
+namespace HashCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client() { Name = "Marcelo", Email = "marcelogomes@gmail.com" };
+            Client b = new Client() { Name = "Maria", Email = "Marcelogomes@gmail.com" };
+
+
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+            Console.WriteLine(a.Equals(b));
+        }
+    }
+}
