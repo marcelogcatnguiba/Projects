f24e94143667c1f294c49808f4a646fe373c3af1
Marcelo Gomes <marcelogomes.0104@gmail.com>
2022-11-21 21:10:10 -0300

    IComparable

---
+++ Generics_02/Generics_02/Entities/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics_02.Entities
+{
+    internal class Product : IComparable
+    {
+        public string Name { get; set; }
+        public double Value { get; set; }
+        public Product()
+        {
+        }
+        public Product(string name, double value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Product))
+            {
+                throw new ArgumentException("Obj is not a product");
+            }
+            Product p = obj as Product;
+
+            return Value.CompareTo(p.Value);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Value}";
+        }
+    }
+}
---
+++ Generics_02/Generics_02/Program.cs
@@ -0,0 +1,34 @@
+﻿using Generics_02.Entities;
+using Generics_02.Services;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Generics_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Product> products = new List<Product>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(',');
+                string name = s[0];
+                double value = double.Parse(s[1], CultureInfo.InvariantCulture);
+
+                products.Add(new Product(name, value));
+            }
+
+            CalculadoraService calculadoraService = new CalculadoraService();
+            Product max = calculadoraService.Max(products);
+
+            Console.Write($"Max : {max}");
+        }
+    }
+}
