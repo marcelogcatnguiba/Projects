05cb2a985f52392425bb17a7cc6f18388cd7db59
Marcelo Gomes <marcelogomes.0104@gmail.com>
2023-05-02 21:35:05 -0300

    Generics-2-Nelio

---
+++ generics-2-nelio/generics-2-nelio/Entities/Products.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics_2_nelio.Entities
+{
+    public sealed class Products : IComparable
+    {
+        public string Name { get; private set; }
+        public int Price { get; private set; }
+
+        public Products(string nome, int preco)
+        {   
+            Valid(nome, preco);
+            Name = nome;
+            Price = preco;
+        }
+
+        public void Update(string nome, int preco)
+        {
+            Valid(nome, preco);
+            Name = nome;
+            Price = preco;
+        }
+        private void Valid(string nome, int preco)
+        {
+            if (string.IsNullOrEmpty(nome))
+            {
+                throw new ArgumentNullException("Não pode ser vazio.");
+            }
+            if(preco <= 0)
+            {
+                throw new ArgumentException("Preco não pode ser vazio.");
+            }
+        }
+        public int CompareTo(object? obj)
+        {
+            if(!(obj is Products))
+            {
+                throw new ArgumentException("Comparer error.");
+            }
+            
+            Products other = (Products)obj;
+            return Price.CompareTo(other.Price);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} : R$ {Price}";
+        }
+    }
+}
---
+++ generics-2-nelio/generics-2-nelio/Program.cs
@@ -0,0 +1,27 @@
+﻿using generics_2_nelio.Entities;
+using generics_2_nelio.Services;
+using System;
+
+try
+{
+    Console.Write("Enter N: ");
+    int n = int.Parse(Console.ReadLine());
+
+    List<Products> list = new List<Products>();
+
+    for (int i = 0; i < n; i++)
+    {
+        string[] vect = Console.ReadLine().Split(',');
+        string product = vect[0];
+        int price = int.Parse(vect[1]);
+
+        list.Add(new Products(product, price));
+    }
+
+    CalculationService calculation = new CalculationService();
+    Console.WriteLine(calculation.Max(list));
+}
+catch (Exception ex)
+{
+
+}
